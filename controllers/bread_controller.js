e495fa62f3a33f6b8699c2e7abf8a7cd84e31fed
Adam Mattheis <oldcartoons@Adams-MBP.attlocal.net>
2022-09-24 13:12:02 -0400

    sending index and show data

--- controllers/bread_controller.js
+++ controllers/bread_controller.js
@@ -2,15 +2,24 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/breads.js')
 
-//index
-breads.get('/', (req,res) => {
-    res.send(Bread)
-})
+
+  // INDEX
+  breads.get('/', (req, res) => {
+    res.render('index',
+    {
+      breads: Bread
+    }
+    
+    )
+  // res.send(Bread)
+  })
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     res.send(Bread[req.params.arrayIndex])
   })
+
+
   
 
 module.exports = breads
