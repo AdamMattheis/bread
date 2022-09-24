1149bb6e5ebd201104466ef9975be82097b9cc2b
Adam Mattheis <oldcartoons@Adams-MBP.attlocal.net>
2022-09-24 11:39:11 -0400

    add

---
+++ index.js
@@ -0,0 +1,25 @@
+//dependencies
+const express = require('express')
+
+
+//configuration
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+
+
+//routes
+app.get('/', (req,res) => {
+    res.send('Welcome to my Bread App!')
+})
+
+//breads
+const breadsController = require('./controllers/bread_controller.js')
+app.use('/breads', breadsController)
+
+//listen
+app.listen(PORT, () => {
+    console.log('listening to port', PORT);
+})
+
